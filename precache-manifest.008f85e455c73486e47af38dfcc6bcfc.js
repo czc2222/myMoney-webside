2d6657f31e16e92420fdf3c32b5a3321bc434081
czc222 <1263291450@qq.com>
2021-11-13 17:25:13 +0800

    deploy

---
+++ precache-manifest.008f85e455c73486e47af38dfcc6bcfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98310e71f1e11f248f64",
+    "url": "/myMoney-webside/css/app.a548412d.css"
+  },
+  {
+    "revision": "d71510785dd39ed29239e1a0cf00579b",
+    "url": "/myMoney-webside/index.html"
+  },
+  {
+    "revision": "98310e71f1e11f248f64",
+    "url": "/myMoney-webside/js/app.49db6eec.js"
+  },
+  {
+    "revision": "4765f4791ac356cf990f",
+    "url": "/myMoney-webside/js/chunk-vendors.e433ce43.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/myMoney-webside/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/myMoney-webside/robots.txt"
+  }
+]);
